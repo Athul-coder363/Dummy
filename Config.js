10b853ffc7110fef57ade92a845a86fa2369ba66
Athul-coder363 <74913344+Athul-coder363@users.noreply.github.com>
2021-12-23 18:17:12 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBspynma6RmFBsuW4HKevyqqqlsQCtGJmo",
+  authDomain: "spectagram-bae21.firebaseapp.com",
+  projectId: "spectagram-bae21",
+  storageBucket: "spectagram-bae21.appspot.com",
+  messagingSenderId: "704555651510",
+  appId: "1:704555651510:web:529ec0627539e93068f117"
+};
